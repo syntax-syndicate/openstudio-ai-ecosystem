1a5f2eb9c92dfde39126246b8a0fe77ee28e09e7
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2025-01-01 11:16:28 +0530

    fix: typo

--- apps/app/app/(authenticated)/page.tsx
+++ apps/app/app/(authenticated)/page.tsx
@@ -40,16 +40,7 @@ const App = async () => {
           </CollaborationProvider>
         )}
       </Header>
-      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          {pages.map((page) => (
-            <div key={page.id} className="aspect-video rounded-xl bg-muted/50">
-              {page.name}
-            </div>
-          ))}
-        </div>
-        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-      </div>
+      <h1>Open Studio</h1>
     </>
   );
 };
