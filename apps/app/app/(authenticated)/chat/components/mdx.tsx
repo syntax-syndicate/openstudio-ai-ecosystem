bf655351fbc459a9adac9e1ad673c9961b5e7c06
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2025-01-13 06:38:40 +0530

    🖼️ fix: correct link card rendering

--- apps/app/app/(authenticated)/chat/components/mdx.tsx
+++ apps/app/app/(authenticated)/chat/components/mdx.tsx
@@ -1,11 +1,14 @@
 import { CodeBlock } from '@/app/(authenticated)/chat/components/codeblock';
+import { SearchFavicon } from '@/app/(authenticated)/chat/components/tools/search-favicon';
 import { REVEAL_ANIMATION_VARIANTS } from '@/helper/animations';
-import { ArrowUpRight, Link } from '@phosphor-icons/react';
+import { ArrowUpRight } from '@phosphor-icons/react';
+import { Flex } from '@repo/design-system/components/ui/flex';
 import {
   HoverCard,
   HoverCardContent,
   HoverCardTrigger,
 } from '@repo/design-system/components/ui/hover-card';
+import { Type } from '@repo/design-system/components/ui/text';
 import { cn } from '@repo/design-system/lib/utils';
 import { motion } from 'framer-motion';
 import Markdown from 'marked-react';
@@ -39,6 +42,7 @@ const Mdx: FC<TMdx> = ({ message, animate, messageId, size = 'base' }) => {
               href={href}
               target="_blank"
               data-message-id={messageId}
+              className="!no-underline font-normal text-blue-500"
               rel="noreferrer"
             >
               {text}
@@ -46,24 +50,22 @@ const Mdx: FC<TMdx> = ({ message, animate, messageId, size = 'base' }) => {
           </HoverCardTrigger>
           <HoverCardContent
             sideOffset={12}
-            className="flex max-w-[500px] cursor-pointer flex-col items-start rounded-xl bg-zinc-700 p-3 hover:bg-zinc-800"
+            className="flex max-w-[400px] cursor-pointer flex-col items-start rounded-lg p-2"
             onClick={() => {
               window.open(href, '_blank');
             }}
           >
-            <p className="flex w-full flex-row items-center gap-2 overflow-hidden whitespace-pre-wrap font-normal text-xs text-zinc-200 leading-7 dark:text-zinc-200">
-              <Link
-                size={16}
-                weight="bold"
-                className="flex-shrink-0 text-white"
-              />
-              {href}
+            <Flex gap="sm" items="start" className="text-zinc-500">
+              <SearchFavicon link={new URL(href).host} className="!m-0 !mt-1" />
+              <Type size="sm" textColor="secondary" className="line-clamp-2">
+                {href}
+              </Type>
               <ArrowUpRight
                 size={16}
                 weight="bold"
-                className="flex-shrink-0 text-white"
+                className="mt-1 flex-shrink-0"
               />
-            </p>
+            </Flex>
           </HoverCardContent>
         </HoverCard>
       );
