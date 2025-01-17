63aa44ece9e081b3f8ba834d13945761f6584f4f
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2025-01-17 08:53:52 +0530

    🔧 chore: update migration output directory in drizzle configuration

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -3,7 +3,7 @@ import { keys } from './keys';
 
 export default defineConfig({
   schema: './schema.ts',
-  out: './',
+  out: './migrations',
   dialect: 'postgresql',
   dbCredentials: {
     url: keys().DATABASE_URL,
