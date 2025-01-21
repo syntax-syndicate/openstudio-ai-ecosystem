db71cc372c7553607dc6657a2720365b597db66e
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2025-01-21 07:49:51 +0530

    🔐 feat: implement sign-up page with OAuth options

---
+++ apps/app/app/sign-up/components/form.tsx
@@ -0,0 +1,169 @@
+'use client';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { microsoftLogin, signup, slackLogin } from '@repo/backend/auth/actions';
+import { Input } from '@repo/design-system/components/precomposed/input';
+import { Prose } from '@repo/design-system/components/prose';
+import { Button } from '@repo/design-system/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@repo/design-system/components/ui/form';
+import { handleError } from '@repo/design-system/lib/handle-error';
+import Link from 'next/link';
+import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+import { z } from 'zod';
+
+const formSchema = z.object({
+  firstName: z.string().min(1).optional(),
+  lastName: z.string().min(1).optional(),
+  email: z.string().email(),
+});
+
+export const SignupForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await signup(values.email, values.firstName, values.lastName);
+      toast.success('Check your email for a signup link.');
+    } catch (error) {
+      handleError(error);
+    }
+  };
+
+  const handleMicrosoftLogin = async () => {
+    try {
+      const url = await microsoftLogin();
+
+      window.location.href = url;
+    } catch (error) {
+      handleError(error);
+    }
+  };
+
+  const handleSlackLogin = async () => {
+    try {
+      const url = await slackLogin();
+
+      window.location.href = url;
+    } catch (error) {
+      handleError(error);
+    }
+  };
+
+  return (
+    <div className="grid w-full gap-4 rounded-lg border bg-background p-8 shadow-sm">
+      <Prose className="prose-sm text-center">
+        <h1 className="m-0 font-semibold text-lg">Create your account</h1>
+        <p className="m-0 text-muted-foreground">
+          Welcome! Please fill in the details to get started.
+        </p>
+      </Prose>
+      <div className="grid gap-2">
+        {/* <div className="grid grid-cols-2 gap-4">
+          <Button variant="outlined" onClick={handleMicrosoftLogin}>
+            <Image
+              src="/icons/microsoft.svg"
+              alt=""
+              width={16}
+              height={16}
+              className="w-4 h-4"
+            />
+            Microsoft
+          </Button>
+          <Button variant="outlined" onClick={handleSlackLogin}>
+            <Image
+              src="/icons/slack.svg"
+              alt=""
+              width={16}
+              height={16}
+              className="w-4 h-4"
+            />
+            Slack
+          </Button>
+        </div>
+        <OrDivider /> */}
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-4">
+            <div className="grid grid-cols-2 gap-4">
+              <FormField
+                control={form.control}
+                name="firstName"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="flex items-center justify-between gap-2">
+                      <FormLabel>First Name</FormLabel>
+                      <p className="text-muted-foreground text-xs">Optional</p>
+                    </div>
+                    <FormControl>
+                      <Input placeholder="Kuluru" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="lastName"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="flex items-center justify-between gap-2">
+                      <FormLabel>Last Name</FormLabel>
+                      <p className="text-muted-foreground text-xs">Optional</p>
+                    </div>
+                    <FormControl>
+                      <Input placeholder="Vineeth" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="kuluruvineeth@openstudio.tech"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              type="submit"
+              disabled={
+                form.formState.disabled ||
+                !form.formState.isValid ||
+                form.formState.isSubmitted ||
+                form.formState.isSubmitting
+              }
+            >
+              Continue
+            </Button>
+          </form>
+        </Form>
+      </div>
+      <Prose className="prose-sm text-center">
+        <p className="text-muted-foreground">
+          Already have an account? <Link href="/sign-in">Sign in</Link>
+        </p>
+      </Prose>
+    </div>
+  );
+};
---
+++ apps/app/app/sign-up/page.tsx
@@ -0,0 +1,51 @@
+import { handleAuthedState } from '@/lib/auth';
+import { Link } from '@repo/design-system/components/link';
+import { Logo } from '@repo/design-system/components/logo';
+import { Prose } from '@repo/design-system/components/prose';
+import { createMetadata } from '@repo/seo/metadata';
+import type { Metadata } from 'next';
+import { Balancer } from 'react-wrap-balancer';
+import { SignupForm } from './components/form';
+
+const title = 'Sign Up';
+const description = 'Sign up to your account';
+
+export const metadata: Metadata = createMetadata({
+  title,
+  description,
+});
+
+const SignUpPage = async () => {
+  await handleAuthedState();
+
+  return (
+    <div className="grid h-screen w-screen divide-x lg:grid-cols-2">
+      <div className="flex items-center justify-center">
+        <div className="w-full max-w-[400px] space-y-8">
+          <Logo className="mx-auto h-12 w-12" />
+          <SignupForm />
+          <Prose>
+            <p className="text-center text-muted-foreground text-sm">
+              <Balancer>
+                By signing in, you agree to our{' '}
+                <Link href="https://www.openstudio.tech/legal/terms">
+                  Terms of Service
+                </Link>{' '}
+                and{' '}
+                <Link href="https://www.openstudio.tech/legal/privacy">
+                  Privacy Policy
+                </Link>
+                .
+              </Balancer>
+            </p>
+          </Prose>
+        </div>
+      </div>
+      <div className="hidden h-full w-full items-start justify-center overflow-hidden bg-background lg:flex">
+        <div className="-m-12 columns-3"></div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
