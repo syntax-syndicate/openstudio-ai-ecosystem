6dea0a4f0990541d83ccdd55b9f7b0c52504d68e
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2025-01-21 18:03:36 +0530

    ➕ add: OrDivider

---
+++ packages/design-system/components/ui/or-divider.tsx
@@ -0,0 +1,7 @@
+export const OrDivider = () => (
+  <div className="flex items-center gap-4">
+    <hr className="flex-1" />
+    <p className="shrink-0 text-muted-foreground text-sm leading-9">OR</p>
+    <hr className="flex-1" />
+  </div>
+);
