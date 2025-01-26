6f9c0943e871fcf11d81d63f623e4f09561617a3
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2025-01-26 12:41:24 +0530

    📥 Add subscriber management and export functionality
    - Created server actions for creating, deleting, and retrieving subscribers
    - Implemented validation schema for subscriber data
    - Added export functionality to generate CSV of subscribers
    - Included access verification and existence checks for subscribers

---
+++ apps/app/actions/subscribers/index.tsx
@@ -0,0 +1,91 @@
+"use server";
+import type * as z from "zod";
+import { database } from "@repo/backend/database";
+import { eq, and, desc } from "drizzle-orm";
+import { subscribers } from "@repo/backend/schema";
+import type { subscribeSchema } from "@/lib/validations/subscribe";
+import { formatVerboseDate } from "@/helper/utils";
+import { json2csv } from "json-2-csv";
+import { ExportResponse } from "@/types/minime";
+
+type SubscriberSchema = z.infer<typeof subscribeSchema>;
+
+export async function createSubscriber(userId: string, data: SubscriberSchema) {
+  const [newSubscriber] = await database.insert(subscribers)
+    .values({ 
+      name: data.name, 
+      email: data.email, 
+      userId 
+    })
+    .returning();
+  return newSubscriber;
+}
+
+export async function deleteSubscriber(subscriberId: string, userId: string) {
+  const [deletedSubscriber] = await database.delete(subscribers)
+    .where(
+      and(
+        eq(subscribers.id, subscriberId),
+        eq(subscribers.userId, userId)
+      )
+    )
+    .returning();
+  return deletedSubscriber.id;
+}
+
+export async function isSubscriberExist(email: string, userId: string) {
+  const count = await database.select()
+    .from(subscribers)
+    .where(
+      and(
+        eq(subscribers.userId, userId),
+        eq(subscribers.email, email)
+      )
+    )
+  return count.length > 0;
+}
+
+export async function verifySubscriberAccess(id: string, userId: string) {
+  const count = await database.select()
+    .from(subscribers)
+    .where(
+      and(
+        eq(subscribers.id, id),
+        eq(subscribers.userId, userId)
+      )
+    )
+  return count.length > 0;
+}
+
+
+export async function getSubscibersExport(
+  userId: string,
+): Promise<ExportResponse> {
+  const subscribersList = await database
+    .select({
+      name: subscribers.name,
+      email: subscribers.email,
+      createdAt: subscribers.createdAt,
+    })
+    .from(subscribers)
+    .where(eq(subscribers.userId, userId));
+
+  const filename = `openstudio_minime_subscribers_export.csv`;
+
+  const content = json2csv(
+    subscribersList.map(({ createdAt, ...subscriber }) => {
+      return { ...subscriber, subscribedAt: formatVerboseDate(createdAt) };
+    }),
+  );
+
+  return { filename, content };
+}
+
+
+export async function getSubscribersByUserId(userId: string) {
+  return await database
+    .select()
+    .from(subscribers)
+    .where(eq(subscribers.userId, userId))
+    .orderBy(desc(subscribers.createdAt));
+}
