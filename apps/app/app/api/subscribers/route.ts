2dd3f570571bfcc226b79f37d5cba64062f2afad
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2025-01-26 12:49:57 +0530

    🌐 Add newsletter subscriber API routes and utility updates
    - Implemented API routes for newsletter subscriber management
    - Added routes for subscribing, deleting, and exporting subscribers
    - Updated Tinybird tracking and user retrieval methods
    - Extended utility types to include subscriber type
    - Implemented rate limiting and validation for subscriber actions

---
+++ apps/app/app/api/subscribers/route.ts
@@ -0,0 +1,71 @@
+import { createSubscriber, isSubscriberExist } from "@/actions/subscribers";
+import { getUserByUsername } from "@repo/backend/auth/utils";
+import { rateLimit } from "@repo/rate-limit";
+import type { NextRequest } from "next/server";
+import * as z from "zod";
+
+const subscribeNewsletterPatchSchema = z.object({
+  name: z.string().min(1),
+  email: z.string().email(),
+  username: z.string().min(1),
+});
+
+export async function POST(req: NextRequest) {
+  try {
+    const data = await req.json();
+
+    const body = subscribeNewsletterPatchSchema.safeParse(data);
+    if (!body.success) {
+      return new Response(body.error.issues[0].message, {
+        status: 422,
+      });
+    }
+
+    const { username, email } = body.data;
+
+    const ip = req.headers.get("x-forwarded-for") || "0.0.0.0";
+
+    if (process.env.VERCEL === "1") {
+      const { success } = await rateLimit.subscribe.limit(
+        `subscribe:${username}:${ip}`,
+      );
+
+      if (!success) {
+        return new Response("Try again after 5 hours.", { status: 429 });
+      }
+    }
+
+    const user = await getUserByUsername(username);
+
+    if (!user) {
+      return new Response("User not found", { status: 404 });
+    }
+    // const { isPro } = await getUserSubscription(user.id);
+
+    // if (!isPro || !user.newsletter) {
+    //   return new Response("This user's newsletter function is not active", {
+    //     status: 401,
+    //   });
+    // }
+
+    if (!user.user_metadata?.newsletter) {
+      return new Response("This user's newsletter function is not active", {
+        status: 401,
+      });
+    }
+
+    const isExist = await isSubscriberExist(email, user.id);
+
+    if (isExist) {
+      return new Response("This email already subscribed", {
+        status: 401,
+      });
+    }
+
+    await createSubscriber(user.id, body.data);
+
+    return new Response(null, { status: 200 });
+  } catch (err) {
+    return new Response(JSON.stringify(err), { status: 500 });
+  }
+}
